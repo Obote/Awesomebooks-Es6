34c2fd1cb0cc3f4632c36f1e6c0121c2ae575831
Obote <obotedenis256@gmail.com>
2022-02-22 22:22:01 +0300

    Add navigation bar

---
+++ modules/navigationbar.js
@@ -0,0 +1,22 @@
+let navSection;
+export default navSection = (navItem) => {
+  const booklist = document.getElementById('books');
+  const addBook = document.getElementById('add-books');
+  const contact = document.getElementById('contact');
+
+  if (navItem === 'list-nav') {
+    booklist.style.display = 'block';
+    addBook.style.display = 'none';
+    contact.style.display = 'none';
+  } else if (navItem === 'add-nav') {
+    booklist.style.display = 'none';
+    addBook.style.display = 'block';
+    contact.style.display = 'none';
+  } else {
+    booklist.style.display = 'none';
+    addBook.style.display = 'none';
+    contact.style.display = 'flex';
+  }
+};
+
+navSection();
