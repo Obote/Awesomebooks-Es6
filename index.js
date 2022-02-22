5683ac4178fea7b830d3721c433a96c2438c6706
Obote <obotedenis256@gmail.com>
2022-02-22 22:25:14 +0300

    Add index file

---
+++ index.js
@@ -0,0 +1,20 @@
+import navSection from './modules/navigationbar.js';
+import BookCollection from './modules/class.js';
+
+navSection('list-nav');
+const navList = ['list-nav', 'add-nav', 'contact-nav'];
+navList.forEach((element) => {
+  const display = document.getElementById(`${element}`);
+  display.addEventListener('click', () => {
+    navSection(element);
+  });
+});
+
+const btnAdd = document.getElementById('btn-add-id');
+const runClass = new BookCollection();
+runClass.displayBooks();
+
+btnAdd.addEventListener('click', () => {
+  runClass.addBooks();
+  runClass.displayBooks();
+});
